8cfe902438828db72c5e680a2081baec9b875740
Hazielnetto <hazielnetto@gmail.com>
2023-11-20 14:14:49 -0300

    Executáveis feitos e run.bat atualizado

--- run.bat
+++ run.bat
@@ -22,11 +22,13 @@ ECHO. >> log.txt
 ECHO        %day%/%month%/%year%  %hour%:%minute%:%second% >> log.txt
 ECHO. >> log.txt
 ECHO.^          ##### Rodando o script Python ##### >> log.txt
-python "Trabalho S.O Buffer.py" %in%
+:: python "Trabalho S.O Buffer.py" %in%
+buffer.exe %in%
 
 ECHO. >> log.txt
 ECHO.^          ##### Rodando a classe Java ##### >> log.txt
-java "Trabalho_SO_Buffer.java" %in%
+:: java "Trabalho_SO_Buffer.java" %in%
+java -jar buffer.jar %in%
 
 ECHO LOG: output.txt 
 ECHO. >> log.txt
