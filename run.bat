6cc378da4cb3b21c03e654b34ad4227e63d68e80
Hazielnetto <hazielnetto@gmail.com>
2023-11-27 17:47:01 -0300

    Correções gerais

--- run.bat
+++ run.bat
@@ -7,26 +7,38 @@ cls
 
 set /p in= "Arquivo de entrada: "
 pause
+set /p loop= "Quantos loops? "
+set "i=1"
+del log.txt
+del log.csv
 
-ECHO. >> log.txt
-ECHO.^          ##### Python ##### >> log.txt
-:: python "Trabalho S.O Buffer.py" %in%
-buffer.exe %in%
+:loop
+if %i% leq %loop% (
+    ECHO --- %i%ª REPETIÇÃO --- >> log.txt
+    ECHO. >> log.txt
+    ECHO.^          ##### Python ##### >> log.txt
+    :: python "Trabalho S.O Buffer.py" %in%
+    bufferPython.exe %in%
 
-ECHO. >> log.txt
-ECHO.^          ##### Java ##### >> log.txt
-:: java "Trabalho_SO_Buffer.java" %in%
-java -jar buffer.jar %in%
+    ECHO. >> log.txt
+    ECHO.^          ##### Java ##### >> log.txt
+    :: java "Trabalho_SO_Buffer.java" %in%
+    java -jar buffer.jar %in%
 
-ECHO. >> log.txt
-ECHO.^          ##### Delphi ##### >> log.txt
-bufferDelphi.exe %in%
+    ECHO. >> log.txt
+    ECHO.^          ##### Delphi ##### >> log.txt
+    echo %in%| bufferDelphi.exe 
+    echo.
 
-ECHO LOG: log.txt & log.csv
-ECHO. >> log.txt
-ECHO.^______________________LOG END______________________ >> log.txt
-ECHO. >> log.txt
+    ECHO LOG: log.txt & log.csv
+    ECHO. >> log.txt
+    ECHO.^______________________LOG END______________________ >> log.txt
+    ECHO. >> log.txt
 
-txtTocsv.exe
+    set /a i+=1
+    goto loop
+)
+
+@REM txtTocsv.exe
 
 pause
