3ae8f05ebc5f746442ea5c6356cc495db04a2410
Hazielnetto <hazielnetto@gmail.com>
2023-11-27 02:01:56 -0300

    Atualização do .bat

--- run.bat
+++ run.bat
@@ -9,14 +9,18 @@ set /p in= "Arquivo de entrada: "
 pause
 
 ECHO. >> log.txt
-ECHO.^          ##### Rodando o script Python ##### >> log.txt
+ECHO.^          ##### Python ##### >> log.txt
 :: python "Trabalho S.O Buffer.py" %in%
 buffer.exe %in%
 
 ECHO. >> log.txt
-ECHO.^          ##### Rodando a classe Java ##### >> log.txt
+ECHO.^          ##### Java ##### >> log.txt
 :: java "Trabalho_SO_Buffer.java" %in%
 java -jar buffer.jar %in%
+
+ECHO. >> log.txt
+ECHO.^          ##### Delphi ##### >> log.txt
+bufferDelphi.exe %in%
 
 ECHO LOG: log.txt & log.csv
 ECHO. >> log.txt
