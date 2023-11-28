ec9916578ed27f3874076cf900736f0c62702c6b
Hazielnetto <hazielnetto@gmail.com>
2023-11-27 23:33:29 -0300

    FINALMENTE RODA AAAAAAAAAAAA

--- teste.c
+++ teste.c
@@ -5,24 +5,12 @@
 
 int main() {
     FILE *inputFile, *outputFile;
-    char nomeArquivo[32], inputFileName[256], outputFileName[256];
+    char nomeArquivo[32], inputFileName[256] = "E:\\Downloads\\GTIP Full-68-v0-30.zip", outputFileName[256] = "E:\\Documentos\\Trabalhos FURB\\Trabalho-S.O\\video2.txt";
     char caminho[1024] = "E:\\Documentos\\Trabalhos FURB\\Trabalho-S.O\\";
-    int TAMANHO_BUFFER = 0;
+    int TAMANHO_BUFFER = 32768;    
 
-    // Get input file name from user
-    printf("Enter the name of the input file: ");
-    scanf("%s", nomeArquivo);     
-    strcat(caminho, nomeArquivo);
-    strcpy(inputFileName, caminho);
-
-    memset(caminho, 0, sizeof(caminho));
-    strcpy(caminho, "E:\\Documentos\\Trabalhos FURB\\Trabalho-S.O\\");
-
-    // Get output file name from user
-    printf("\nEnter the name of the output file: ");
-    scanf("%s", nomeArquivo);
-    strcat(caminho, nomeArquivo);
-    strcpy(outputFileName, caminho);
+    printf("Caminho input: %s", inputFileName);
+    printf("Caminho output: %s", outputFileName);
 
     inputFile = fopen(inputFileName, "rb");
     if (inputFile == NULL) {
@@ -30,38 +18,36 @@ int main() {
         return 1;
     }
     
-    outputFile = fopen(caminho, "w");
+    outputFile = fopen(outputFileName, "w");
     if (outputFile == NULL) {
         printf("Error opening output file.\n");
         return 1;
     }
 
     // Read from input file and write to output file using buffer
-    char buffer[32768];
+    // char buffer[32768];
+    char buffer[TAMANHO_BUFFER];
     size_t bytesRead;
+    long l = 0;
+    
+    while (TAMANHO_BUFFER >= 4)
+    {
+        long startTime = time(NULL);        
 
-    fseek(inputFile, 0, SEEK_END);
-    long ofilesize = ftell(inputFile);
-    fclose(inputFile);
+        while ((bytesRead = fread(buffer, 1, sizeof(buffer), inputFile)) > 0) 
 
-    //while (TAMANHO_BUFFER >= 4){
-        long startTime = time(NULL);
-
-        while ((bytesRead = fread(buffer, 1, sizeof(buffer), inputFile)) > 0) {
-                fwrite(buffer, 1, bytesRead, outputFile);
-            }
+        {                
+            fwrite(buffer, 1, bytesRead, outputFile);
+        }
 
         float elapsedTime = (float)(time(NULL) - startTime);
 
         printf("\nNome do arquivo: %s\n", inputFileName);
-        printf("Tamanho do arquivo: %ld Bytes\n", ofilesize);
+        printf("Tamanho do arquivo: Bytes\n");
         printf("Tempo de execucao:  %.2f segundos\n", elapsedTime);
-        printf("Tamanho do buffer:  %d Bytes\n\n", TAMANHO_BUFFER);
+        printf("Tamanho do buffer:  %ld Bytes\n\n", TAMANHO_BUFFER);
         TAMANHO_BUFFER *= 0.5; 
-    //}
-        // while ((bytesRead = fread(buffer, 1, sizeof(buffer), inputFile)) > 0) {
-        // fwrite(buffer, 1, bytesRead, outputFile);
-        // }
+    }        
 
     fclose(inputFile);
     fclose(outputFile);
