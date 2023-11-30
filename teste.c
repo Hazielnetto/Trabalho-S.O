652466415e3ebf012f65812f4db3c0dd320ad035
Hazielnetto <hazielnetto@gmail.com>
2023-11-30 02:03:49 -0300

    adicionado flush

--- teste.c
+++ teste.c
@@ -1,28 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 // Modelo 1 de read-write em C
 
 int main() {
     FILE *inputFile, *outputFile;
-    char nomeArquivo[32], inputFileName[256] = "E:\\Documentos\\Trabalhos FURB\\Trabalho-S.O\\video.mp4", outputFileName[256] = "E:\\Documentos\\Trabalhos FURB\\Trabalho-S.O\\video.txt";
-    char caminho[1024] = "E:\\Documentos\\Trabalhos FURB\\Trabalho-S.O\\";
+    char inputFileName[256] = "C:\\Users\\haziel\\Documents\\Trabalhos FURB\\Trabalho-S.O\\files\\video480p.mp4";
+    char outputFileName[256] = "C:\\Users\\haziel\\Documents\\Trabalhos FURB\\Trabalho-S.O\\files\\video480p.mp4.txt";
     int TAMANHO_BUFFER = 32768;    
 
     printf("Caminho input: %s", inputFileName);
     printf("Caminho output: %s", outputFileName);
-
-    inputFile = fopen(inputFileName, "rb");
-    if (inputFile == NULL) {
-        printf("Error opening input file.\n");
-        return 1;
-    }
     
-    outputFile = fopen(outputFileName, "wb");
-    if (outputFile == NULL) {
-        printf("Error opening output file.\n");
-        return 1;
-    }
 
     // Read from input file and write to output file using buffer
     // char buffer[32768];
@@ -31,20 +21,30 @@ int main() {
     
     while (TAMANHO_BUFFER >= 4){
 
-        long startTime = time(NULL);        
+        inputFile = fopen(inputFileName, "rb");
+        // if (inputFile == NULL) {
+        //     printf("Error opening input file.\n");
+        //     return 1;
+        // }
+    
+        outputFile = fopen(outputFileName, "wb");
+        // if (outputFile == NULL) {
+        //  printf("Error opening output file.\n");
+        //     return 1;
+        // }
 
-        do{
+        clock_t startTime = clock();        
 
+        while ((bytesRead = fread(buffer, 1, sizeof(buffer), inputFile)) > 0){
             fwrite(buffer, 1, bytesRead, outputFile);
-            printf("asidbauwydbawd awd\n");
+            fflush(inputFile);            
+        }
 
-        }while ((bytesRead = fread(buffer, 1, sizeof(buffer), inputFile)) > 0);
-
-        float elapsedTime = (float)(time(NULL) - startTime);
+        double elapsedTime = ((double)startTime)/CLOCKS_PER_SEC;
 
         printf("\nNome do arquivo: %s\n", inputFileName);
         printf("Tamanho do arquivo: Bytes\n");
-        printf("Tempo de execucao:  %.2f segundos\n", elapsedTime);
+        printf("Tempo de execucao:  %.3f segundos\n", elapsedTime);
         printf("Tamanho do buffer:  %ld Bytes\n\n", TAMANHO_BUFFER);
         TAMANHO_BUFFER *= 0.5; 
     }        
